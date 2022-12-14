4e25a2b1b9ddbf0b3e5cb391b6110f45acb6180f
shuaibaozhang <2742520302@qq.com>
2022-12-14 22:05:03 +0800

    完成了home页功能的开发

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 import Vue from 'vue'
 import App from './App'
 
+//导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+uni.$http=$http
+
+$http.baseUrl='https://www.uinav.com'
+//请求拦截器
+$http.beforeRequest=function(options){
+  uni.showLoading({
+    title:'数据加载中',
+  })
+}
+
+//相应拦截器
+$http.afterRequest=function(){
+  uni.hideLoading()
+}
+//封装弹框提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
